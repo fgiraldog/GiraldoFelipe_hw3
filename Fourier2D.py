a14b0d4651662acd2cd1c65ca05d816935e9291a
Felipe Giraldo <fgiraldo@f.giraldo>
2018-10-28 18:14:02 -0500

    Filtro arreglado e implementacion de fftshift

--- Fourier2D.py
+++ Fourier2D.py
@@ -5,18 +5,14 @@ from matplotlib.colors import LogNorm
 
 imagen = plt.imread('arbol.png')
 
+imagen_transformada = fftpack.fft2(imagen)
+
+transformada_centrada = fftpack.fftshift(imagen_transformada)
+
+print(np.shape(transformada_centrada))
 
 plt.figure()
-plt.imshow(imagen,plt.cm.gray)
-plt.title('Imagen original')
-plt.show()
-
-im_fft = fftpack.fft2(imagen)
-
-print(im_fft)
-
-plt.figure()
-plt.imshow(np.log(np.abs(im_fft)))
+plt.imshow(np.abs(transformada_centrada), norm = LogNorm(vmin=4))
 plt.colorbar()
 plt.title('Transformada de Fourier')
 plt.show()
@@ -24,22 +20,25 @@ plt.show()
 def filtro(matrix):
 	for i in range(0,np.shape(matrix)[0]):
 		for j in range(0,np.shape(matrix)[1]):
-			if np.log(matrix[i,j]) > 7.75:
+			if (104 < i < 152) and (104 < j < 152):
+				matrix[i,j] = matrix[i,j]
+			elif matrix[i,j] > 175:
 				matrix[i,j] = 0
+				
 	return matrix
 
-transformada_filtrada = filtro(im_fft)
+transformada_centrada2 = transformada_centrada.copy()
+transformada_filtrada = filtro(transformada_centrada2)
 
 plt.figure()
-plt.imshow(transformada_filtrada,norm=LogNorm())
+plt.imshow(np.abs(transformada_filtrada), norm = LogNorm(vmin=4))
 plt.title('Transformada de Fourier Filtrada')
 plt.show()
 
-imagen_nueva = fftpack.ifft2(transformada_filtrada)
+imagen_nueva = fftpack.ifft2(fftpack.ifftshift(transformada_filtrada))
 
 plt.figure()
 plt.imshow(np.abs(imagen_nueva),plt.cm.gray)
-plt.colorbar()
 plt.title('Imagen Filtrada')
 plt.show()
 
