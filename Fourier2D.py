361d9c89a7cce54f956e532d4ab87a3ff008c101
Felipe Giraldo <fgiraldo@f.giraldo>
2018-10-29 08:48:46 -0500

    Version completa sin comentarios

--- Fourier2D.py
+++ Fourier2D.py
@@ -3,19 +3,17 @@ import matplotlib.pyplot as plt
 from scipy import fftpack
 from matplotlib.colors import LogNorm
 
-imagen = plt.imread('arbol.png')
+imagen = np.array(plt.imread('arbol.png'))
 
 imagen_transformada = fftpack.fft2(imagen)
 
 transformada_centrada = fftpack.fftshift(imagen_transformada)
 
-print(np.shape(transformada_centrada))
-
 plt.figure()
-plt.imshow(np.abs(transformada_centrada), norm = LogNorm(vmin=4))
+plt.imshow(np.log(np.abs(transformada_centrada)))
 plt.colorbar()
-plt.title('Transformada de Fourier')
-plt.show()
+plt.title('Transformada de Fourier (Escala logaritimica)')
+plt.savefig('GiraldoFelipe_FT2D.pdf')
 
 def filtro(matrix):
 	for i in range(0,np.shape(matrix)[0]):
@@ -33,14 +31,15 @@ transformada_filtrada = filtro(transformada_centrada2)
 plt.figure()
 plt.imshow(np.abs(transformada_filtrada), norm = LogNorm(vmin=4))
 plt.title('Transformada de Fourier Filtrada')
-plt.show()
+plt.colorbar()
+plt.savefig('GiraldoFelipe_FT2D_filtrada.pdf')
 
 imagen_nueva = fftpack.ifft2(fftpack.ifftshift(transformada_filtrada))
 
 plt.figure()
 plt.imshow(np.abs(imagen_nueva),plt.cm.gray)
 plt.title('Imagen Filtrada')
-plt.show()
+plt.savefig('GiraldoFelipe_Imagen_filtrada.pdf')
 
 
 
