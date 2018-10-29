b81c6a6ce05b5b2aebf0c33582adc01af27e3164
Felipe Giraldo <fgiraldo@f.giraldo>
2018-10-28 20:07:20 -0500

    Verificacion del codigo usando paquetes de sklearn

--- PCR.py
+++ PCR.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
+from sklearn.decomposition import PCA
 import os
 
 comando_1 = 'sed -i "s/M/1/g" WDBC.dat' 
@@ -13,7 +14,7 @@ info_paciente = np.genfromtxt('WDBC.dat', delimiter = ',', usecols = (0,1))
 def normalizacion(datos_1):
 	N = np.shape(datos_1)[1]
 	for i in range(0, N):
-		datos[:,i] = datos[:,i]/np.std(datos[:,i])
+		datos_1[:,i] = (datos_1[:,i]-np.mean(datos_1[:,i]))/np.std(datos_1[:,i])
 
 	return datos_1	
 
@@ -28,31 +29,43 @@ def matriz_covarianza(datos_1):
 
 matriz_cov = matriz_covarianza(normalizacion(datos))
 matriz_numpy = np.cov(np.transpose(normalizacion(datos)))
-correccion = abs(matriz_cov) - abs(matriz_numpy)
+
+def verificacion_cov(matriz_mia,matriz_numpy):
+	N = np.shape(matriz_mia)[1]
+	contador = 0
+	for i in range(0, N):
+		for j in range(0, N):
+			pos_mia = matriz_mia[i,j]
+			pos_numpy = matriz_numpy[i,j]
+			if(pos_numpy < 0 and pos_mia > 0):
+				contador = contador + 1
+			elif(pos_numpy > 0 and pos_mia < 0):
+				contador = contador + 1
+			elif(abs(pos_numpy)-abs(pos_mia) > 10**-3):
+				contador = contador + 1
+	if contador > 0:
+		print('La matriz de covarianza propia esta mal')
+	else:
+		print('La matriz de covarianza propia esta bien')
+			
 
 print('La matriz de covarianzas es:')
 print(matriz_cov)
-
-#print(matriz_numpy)
-
-#print(correccion)
+verificacion_cov(matriz_cov,matriz_numpy)
 
 valores_propios, vectores_propios = np.linalg.eig(matriz_cov)
 
-#for i in range(0,len(valores_propios)):
-#	print ('El valor propio y el vector propio ', i+1, ' son:')
-#	print (valores_propios[i])
-#	print (vectores_propios[:,i])
+for i in range(0,len(valores_propios)):
+	print ('El valor propio y el vector propio ', i+1, ' son:')
+	print (valores_propios[i])
+	print (vectores_propios[:,i])
 
-#print ('Los valores propios son: ', max(valores_propios))
-
-#print ('Los vectores propios son: ', vectores_propios)
 
 vector_prop_1 = vectores_propios[:,0]
 vector_prop_2 = vectores_propios[:,1]
 
-PC1 = np.dot(vector_prop_1,np.transpose(datos))
-PC2 = np.dot(vector_prop_2,np.transpose(datos))
+PC1 = np.dot(vector_prop_1,np.transpose(normalizacion(datos)))
+PC2 = np.dot(vector_prop_2,np.transpose(normalizacion(datos)))
 
 def benigno_maligno(b_n,pc1,pc2):
 	benigno_pc1 = []
@@ -70,6 +83,7 @@ def benigno_maligno(b_n,pc1,pc2):
 	return np.array(maligno_pc1), np.array(maligno_pc2), np.array(benigno_pc1), np.array(benigno_pc2)
 
 maligno_PC1, maligno_PC2, benigno_PC1, benigno_PC2 = benigno_maligno(info_paciente[:,1], PC1, PC2)
+
 plt.figure()
 plt.scatter(maligno_PC1,maligno_PC2, label = 'Diagnostico Maligno')
 plt.scatter(benigno_PC1,benigno_PC2, label = 'Diagnostico Benigno')
@@ -79,4 +93,12 @@ plt.legend()
 plt.show()
 
 
+#pca = PCA(n_components=2)
+#principalComponents = pca.fit_transform((datos))
 
+#plt.figure()
+#plt.scatter(principalComponents[:,0], principalComponents[:,1], label = 'Sklearn')
+#plt.ylabel('PC2')
+#plt.xlabel('PC1')
+#plt.legend()
+#plt.show()
