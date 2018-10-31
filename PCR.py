c77de7854105a7bc146eaabba95b621ffba738b8
Felipe Giraldo <fgiraldo@f.giraldo>
2018-10-31 14:26:44 -0500

    Version final

--- PCR.py
+++ PCR.py
@@ -3,14 +3,14 @@
 #Importacion de los paquetes a usar
 import numpy as np
 import matplotlib.pylab as plt
-from sklearn.decomposition import PCA #Este paquete se usa para verificar que el PCA se hizo de manera correcta
-import os #Para poder usar bash en el codigo
+#from sklearn.decomposition import PCA #Este paquete se usa para verificar que el PCA se hizo de manera correcta (Descomentar si se quiere verificar)
+import os as bash #Para poder usar bash en el codigo
 
 #Adecuacion de la base de datos 
 comando_1 = 'sed -i "s/M/1/g" WDBC.dat' 
 comando_2 = 'sed -i "s/B/0/g" WDBC.dat'
-os.system(comando_1)
-os.system(comando_2)
+bash.system(comando_1)
+bash.system(comando_2)
 
 #Importacion de los datos
 datos = np.genfromtxt('WDBC.dat', delimiter = ',', usecols = (2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)) #Los parametros de cada paciente
@@ -61,9 +61,8 @@ def verificacion_cov(matriz_mia,matriz_numpy):
 print('La matriz de covarianzas es:')
 print(matriz_cov)
 
-print('--------------------------------------------------------------------------------')
-
-#verificacion_cov(matriz_cov,matriz_numpy) #Si se quiere comprobar que la matriz esta bien, descomentar esta linea
+#print('--------------------------------------------------------------------------------')
+#verificacion_cov(matriz_cov,matriz_numpy) #Si se quiere comprobar que la matriz esta bien, descomentar estas lineas
 
 print('--------------------------------------------------------------------------------')
 
@@ -79,7 +78,7 @@ for i in range(0,len(valores_propios)):
 print('--------------------------------------------------------------------------------')
 
 #Comentario de los componentes mas importantes en este analisis
-print('Segun los resultados recien presentados, para cada autovalor y cada autovector, se puede ver que los componentes o parametros mas importantes son PC1 y PC2, es decir el primer autovector y el segundo autovector. Esto se puede confirmar haciendo uso del porcentaje de la varianza que estos atribuyen a los datos. Entonces, PC1 es responsable del ', valores_propios[0]*100/np.sum(valores_propios), '% de la varianza, y PC2 es responsable del ', valores_propios[1]*100/np.sum(valores_propios), '% de la varianza.')
+print('Segun los resultados recien presentados, para cada autovalor y cada autovector, se puede ver que los componentes mas importantes son PC1 y PC2, es decir el primer autovector y el segundo autovector. Esto se puede confirmar haciendo uso del porcentaje de la varianza que estos atribuyen a los datos. Entonces, PC1 es responsable del ', valores_propios[0]*100/np.sum(valores_propios), '% de la varianza, y PC2 es responsable del ', valores_propios[1]*100/np.sum(valores_propios), '% de la varianza. Ahora bien, en cuanto a los parametros de cada auto vector, en PC1 se ve claramente que la mayoria de estos tiene el mismo peso, pero el mayor se encuentra en la posicion [7] con un valor de 0.26085376 y en la hoja de los datos, esto corresponde a concave points (number of concave portions of the contour). Ahora bien, en cuanto al autovector correspondiente a PC2, se puede apreciar que se cumple el mismo fenomeno que cada parametro tiene el mismo peso, pero el mayor se encuentra en la posicion [9] con un valor de 0.36657547, y en la hoja de datos corresponde a fractal dimension ("coastline approximation" - 1).')
 
 #Definicion de los vectores propios para PC1 y PC2
 vector_prop_1 = vectores_propios[:,0]
@@ -110,8 +109,8 @@ maligno_PC1, maligno_PC2, benigno_PC1, benigno_PC2 = benigno_maligno(info_pacien
 
 #Grafica de PC1 y PC2 con los diagnoticos en diferentes colores
 plt.figure()
-plt.scatter(maligno_PC1,maligno_PC2, label = 'Diagnostico Maligno')
-plt.scatter(benigno_PC1,benigno_PC2, label = 'Diagnostico Benigno')
+plt.scatter(maligno_PC1,maligno_PC2, c = 'g', label = 'Diagnostico Maligno')
+plt.scatter(benigno_PC1,benigno_PC2, c = 'b', label = 'Diagnostico Benigno')
 plt.ylabel('PC2')
 plt.xlabel('PC1')
 plt.legend()
@@ -120,7 +119,7 @@ plt.savefig('GiraldoFelipe_PCA.pdf')
 print('--------------------------------------------------------------------------------')
 
 #Comentario acerca de los resultados obtenidos
-print('Para termiar con este punto, es importante analizar los resultados obtenidos de la proyeccion de los datos en los ejes de PC1 y PC2. Entonces, como se puede ver en la grafica que ha sido guardada en su computador GiraldoFelipe_PCA.pdf, los diagnosticos benignos estan agrupados a la derecha del grafico, y tienen una varianza menor en cuanto al eje PC1 que aquella para los diagnosticos malignos. Asi mismo, se puede ver que los diagnostico malignos tienen valores mayores en cuanto al eje PC1 y estan dispersos en cuanto a ambos ejes. Pero de manera general, se puede apreciar una clara diferencia en la agrupacion de estos datos, en donde los diagnosticos benignos estan hacia la izquierda y los malignos esta corridos un poco hacia la derecha. Asi pues, con esto resultados se puede decir que el analisis de PCA, para este caso probo ser exitososo debido a que se pudo diferenciar los casos en donde el diagnostico es benigno o maligno, lo cual podria ayudar a los paciente e incluso a los doctores a tener una mayor precuacion con diagnosticos que se encuentren en la zona de los malignos. ')
+print('Para termiar con este punto, es importante analizar los resultados obtenidos de la proyeccion de los datos en los ejes de PC1 y PC2. Entonces, como se puede ver en la grafica que ha sido guardada en su computador GiraldoFelipe_PCA.pdf, los diagnosticos benignos estan agrupados a la izquierda del grafico, y tienen una varianza menor en cuanto al eje PC1 que aquella para los diagnosticos malignos. Asi mismo, se puede ver que los diagnostico malignos tienen valores mayores en cuanto al eje PC1 y estan dispersos en cuanto a ambos ejes. Pero de manera general, se puede apreciar una clara diferencia en la agrupacion de estos datos, en donde los diagnosticos benignos estan hacia la izquierda y los malignos esta corridos un poco hacia la derecha. Asi pues, con esto resultados se puede decir que el analisis de PCA, para este caso probo ser exitoso debido a que se pudo diferenciar los casos en donde el diagnostico es benigno o maligno, lo cual podria ayudar a los paciente e incluso a los doctores a tener una mayor precaucion con diagnosticos que se encuentren en la zona de los malignos. ')
 
 
 #Si se quiere comprobar que el PCA fue hecho de manera correcta, se puede descomentar esta seccion. Aqui no se hace diferencia entre los diagnosticos pero se ve que laforma obtenida en la grafica es igual a aquella encontrada en la implementacion propia
